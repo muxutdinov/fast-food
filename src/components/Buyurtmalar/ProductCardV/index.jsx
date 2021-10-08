bbcc9eb3bf4b4212cee09e1bfb02f78e6570599e
Jaxongir <jmuxutdinov@mail.ru>
2021-10-08 08:04:36 +0500

    create second card

--- src/components/Buyurtmalar/ProductCardV/index.jsx
+++ src/components/Buyurtmalar/ProductCardV/index.jsx
@@ -16,6 +16,22 @@ import {
   Time,
   Border,
   Last,
+  Wrapikki,
+  User,
+  Name,
+  Phone,
+  Tel,
+  Wrapuch,
+  Sum,
+  Narx,
+  Truck,
+  IconWraps,
+  Umumiy,
+  Number,
+  Oval,
+  Payme,
+  Wraptort,
+  Oprname,
 } from "./style";
 import { BuyurtmaContext } from "../../context/Buyurtmalar/buyurtmalar";
 
@@ -34,19 +50,65 @@ export const ProductCard = ({ value }) => {
         <MinWrapbir>
           <Wrapbir align>
             <Num>{value.orderId}</Num>
-            <Border/>
-            <IconWrap><Save/></IconWrap>
+            <Border />
+            <IconWrap>
+              <Save />
+            </IconWrap>
           </Wrapbir>
           <Wrapbir>
-            <Soat/>
+            <Soat />
             <Time>00:34</Time>
           </Wrapbir>
         </MinWrapbir>
-        <Last/>
-        <MinWrapikki>2</MinWrapikki>
-        <MinWrapuch>3</MinWrapuch>
-        <MinWraptort>4</MinWraptort>
+        <Last />
+        <MinWrapikki>
+          <Wrapikki>
+            <User />
+            <Name>{value.user.name}</Name>
+          </Wrapikki>
+          <Wrapikki>
+            <Phone />
+            <Tel>(+99893) 461-41-88</Tel>
+          </Wrapikki>
+        </MinWrapikki>
+        <Last />
+        <MinWrapuch>
+          <IconWraps>
+            <Wrapuch>
+              <Sum />
+              <Narx>{value.total} UZS</Narx>
+            </Wrapuch>
+            <Wrapuch>
+              <Truck />
+              <Narx truck>{value.total} UZS</Narx>
+            </Wrapuch>
+          </IconWraps>
+          <Wrapuch number>
+            <Umumiy>Umumiy summa</Umumiy>
+            <Number>{value.total} UZS</Number>
+          </Wrapuch>
+          <Payme>
+            <Oval /> <div className="pay">Payme</div>
+          </Payme>
+        </MinWrapuch>
+        <Last />
+        <MinWraptort>
+          <Wraptort>
+            <Umumiy>Operator:</Umumiy>
+            <Oprname>{value.operator.name}</Oprname>
+          </Wraptort>
+          <Wraptort>
+            <Umumiy>Filial:</Umumiy>
+            <Oprname>
+              {value.filial.title}
+              <br />
+              {value.filial.location}
+            </Oprname>
+          </Wraptort>
+        </MinWraptort>
       </Wrapper>
+        <Cancel />
+        <Done />
     </Container>
   );
 };
--- src/components/Buyurtmalar/ProductCardV/style.js
+++ src/components/Buyurtmalar/ProductCardV/style.js
@@ -2,9 +2,14 @@ import styled from "styled-components";
 import { ReactComponent as save } from "../../../assets/icons/save.svg";
 import { ReactComponent as clock } from "../../../assets/icons/clock.svg";
 import { ReactComponent as user } from "../../../assets/icons/user.svg";
+import { ReactComponent as phone } from "../../../assets/icons/phone.svg";
+import { ReactComponent as truck } from "../../../assets/icons/truck.svg";
+import { ReactComponent as sum } from "../../../assets/icons/sum.svg";
+import { ReactComponent as oval } from "../../../assets/icons/Oval.svg";
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   margin-bottom: 20px;
 `;
 
@@ -23,13 +28,12 @@ export const MinWrapbir = styled.div`
   display: flex;
   justify-content: center;
   flex: 1;
-  /* border-right: 2px solid  #EDEFF3; */
   padding-left: 10px;
   padding-right: 10px;
 `;
 export const Wrapbir = styled.div`
-position: relative;
-margin-bottom: 20px;
+  position: relative;
+  margin-bottom: 20px;
   display: flex;
 `;
 export const Num = styled.div`
@@ -44,17 +48,17 @@ export const Num = styled.div`
   margin-right: 20px;
   margin-left: 25px;
   margin-bottom: 10px;
-  `;
+`;
 export const Border = styled.div`
-position: absolute;
-top: 40px;
-left: 20px;
-width: 84px;
-height: 1px;
-mix-blend-mode: normal;
-opacity: 0.2;
-border: 1px solid #979797;
-`
+  position: absolute;
+  top: 40px;
+  left: 20px;
+  width: 84px;
+  height: 1px;
+  mix-blend-mode: normal;
+  opacity: 0.2;
+  border: 1px solid #979797;
+`;
 export const IconWrap = styled.div`
   display: flex;
   align-items: center;
@@ -63,34 +67,159 @@ export const IconWrap = styled.div`
   height: 30px;
   background: #edeff3;
   border-radius: 15px;
-  `;
+`;
 export const Save = styled(save)`
   width: 12.44px;
   height: 16px;
-  `;
+`;
 export const Soat = styled(clock)`
-width: 16px;
-height: 16px;
-margin-left: 25px;
-`
+  width: 16px;
+  height: 16px;
+  margin-left: 25px;
+`;
 export const Time = styled.div`
-font-family: SFProDisplay;
-font-size: 16px;
-line-height: 16px;
-color: #2D3A45;
-margin-left: 20px;
-`
+  font-family: SFProDisplay;
+  font-size: 16px;
+  line-height: 16px;
+  color: #2d3a45;
+  margin-left: 20px;
+`;
 export const Last = styled.div`
-width: 2px;
-height: 150px;
-background: #EDEFF3;
-`
+  width: 2px;
+  height: 150px;
+  background: #edeff3;
+`;
 export const MinWrapikki = styled.div`
   flex: 1.4;
+  display: flex;
+  flex-direction: column;
 `;
+export const Wrapikki = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+`;
+export const User = styled(user)`
+  width: 14.22px;
+  height: 16px;
+  mix-blend-mode: normal;
+  opacity: 0.5;
+  margin-right: 40px;
+  `;
+export const Name = styled.div`
+font-family: SFProDisplay;
+font-size: 20px;
+line-height: 20px;
+letter-spacing: 0.666667px;
+color: #2D3A45;
+margin-bottom: 30px;
+`
+export const Phone = styled(phone)`
+margin-right: 40px;
+width: 15.15px;
+height: 15.18px;
+opacity: 0.5;
+`
+export const Tel = styled.div`
+width: 144px;
+height: 18px;
+font-family: SFProDisplay;
+font-size: 14px;
+line-height: 18px;
+letter-spacing: 0.466667px;
+color: #2D3A45;
+`
 export const MinWrapuch = styled.div`
   flex: 1.4;
+  position: relative;
 `;
+export const IconWraps = styled.div`
+display: flex;
+flex-direction: column;
+margin-right: 120px;
+margin-bottom: 30px;
+`
+export const Wrapuch = styled.div`
+display: flex;
+flex-direction: ${({number})=>number?"column":""};
+justify-content: center;
+margin-left:${({number})=>number?"30px":""};
+`
+export const Sum = styled(sum)`
+width: 12.8px;
+height: 16px;
+mix-blend-mode: normal;
+opacity: 0.5;
+margin-right: 10px;
+`
+export const Narx = styled.div`
+top: calc(50% - 18px/2);
+font-family: SFProDisplay;
+font-size: 14px;
+line-height: 18px;
+letter-spacing: 0.466667px;
+color: #2D3A45;
+margin-top: ${({truck})=>truck?"15px":""};
+`
+export const Truck = styled(truck)`
+margin-top: 15px;
+width: 16px;
+height: 13.09px;
+mix-blend-mode: normal;
+opacity: 0.5;
+margin-right: 10px;
+`
+export const Umumiy = styled.div`
+font-family: SFProDisplay;
+font-size: 11px;
+line-height: 18px;
+letter-spacing: 0.366667px;
+color: #8D9BA8;
+`
+export const Number = styled.div`
+margin-top: 10px;
+font-family: SFProDisplay;
+font-size: 20px;
+line-height: 2px;
+letter-spacing: 0.666667px;
+color: #2D3A45;
+`
+export const Payme = styled.div`
+position: absolute;
+display: flex;
+flex-direction: row;
+align-items: center;
+bottom: 90px;
+left: 190px;
+.pay{
+  font-weight: 200;
+}
+`
+export const Oval = styled(oval)`
+border-radius: 50%;
+background: #14E5E4;
+height: 10px;
+width: 10px;
+margin-right: 10px;
+`
 export const MinWraptort = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
+export const Wraptort = styled.div`
+margin-top: 10px;
+margin-left: 30px;
+margin-bottom: 10px;
+display: flex;
+flex-direction: column;
+`
+export const Oprname = styled.div`
+font-weight: 500;
+font-family: SFProDisplay;
+font-size: 14px;
+line-height: 18px;
+letter-spacing: 0.466667px;
+color: #2D3A45;
+`
